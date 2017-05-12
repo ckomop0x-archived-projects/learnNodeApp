974fa216d931d4b20095233d3d0834cf1302696a
Pavel Klochkov <paul.klochkov@gmail.com>
2017-05-12 23:57:14 +0300

    Save checkpoint

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,3 +1,6 @@
+const mongoose = require('mongoose');
+const Store = mongoose.model('Store')
+
 exports.homePage = (req, res) => {
 	console.log(11)
 	res.render('index')
@@ -5,4 +8,14 @@ exports.homePage = (req, res) => {
 
 exports.addStore = (req, res) => {
 	res.render('editStore', { title: 'Add Store' })
+};
+
+exports.createStore = (req, res) => {
+	const store = new Store(req.body)
+	store.save(function(err, store) {
+		if(!err){
+			console.log('It worked!')
+			res.redirect('/');
+		}
+	});
 };
