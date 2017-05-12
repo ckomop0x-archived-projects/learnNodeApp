ee704ef56bd0306c82a0abad16a91767f341fc93
Pavel Klochkov <paul.klochkov@gmail.com>
2017-05-13 00:29:28 +0300

    Files policy

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -10,12 +10,8 @@ exports.addStore = (req, res) => {
 	res.render('editStore', { title: 'Add Store' })
 };
 
-exports.createStore = (req, res) => {
-	const store = new Store(req.body)
-	store.save(function(err, store) {
-		if(!err){
-			console.log('It worked!')
-			res.redirect('/');
-		}
-	});
+exports.createStore = async (req, res) => {
+	const store = new Store(req.body);
+	await store.save()
+	res.redirect('/')
 };
