2a4760492aef334a6e0c248ee8eebf959a5a8a0d
Pavel Klochkov <paul.klochkov@gmail.com>
2017-05-13 20:43:38 +0300

    Add photos to store cards

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,14 +1,55 @@
 const mongoose = require('mongoose');
-const Store = mongoose.model('Store')
+const Store = mongoose.model('Store');
+const multer = require('multer');
+const jimp = require('jimp');
+const uuid = require('uuid');
 
+const multerOptions = {
+	storage: multer.memoryStorage(),
+	fileFilter(req, file, next) {
+		const isPhoto = file.mimetype.startsWith('image/');
+		if(isPhoto) {
+			next(null, true);
+		} else {
+			next({ message: 'That filetype isn\'t allowed!' }, false);
+		}
+	}
+};
+
+// Home page
 exports.homePage = (req, res) => {
 	res.render('index')
-}
+};
 
+
+// Add a new store
 exports.addStore = (req, res) => {
 	res.render('editStore', { title: 'Add Store' })
 };
 
+// Image upload
+exports.upload = multer(multerOptions).single('photo');
+
+// Resize photos
+exports.resize = async (req, res, next) => {
+	// Check if there is no new file to resize
+	if( !req.file ){
+		next(); // skip to the next middleware
+		return;
+	}
+	const extension = req.file.mimetype.split('/')[1];
+	req.body.photo = `${uuid.v4()}.${extension}`;
+
+	// now we will resize photo
+	const photo = await jimp.read(req.file.buffer);
+	await photo.resize(800, jimp.AUTO);
+	await photo.write(`./public/uploads/${req.body.photo}`);
+
+	// once we have written photo to our filesystem, keep going!
+	next();
+}
+
+// Create a new sotre
 exports.createStore = async (req, res) => {
 	const store = await (new Store(req.body)).save();
 	req.flash('success', `Successfully Created ${store.name}. Care to leave a review?`);
@@ -31,6 +72,8 @@ exports.editStore = async (req, res) => {
 };
 
 exports.updateStore = async (req, res) => {
+	// 0. Set the location data to be a point
+	req.body.location.type = 'Point';
 	// 1. Find and update the store
 	const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
 		new: true, // return the new store instead of the old one
