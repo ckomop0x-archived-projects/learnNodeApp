0c05a9f402d9728d0a79a95a1466078f4b19a7ce
Pavel Klochkov <paul.klochkov@gmail.com>
2017-05-14 14:28:54 +0300

    creating users accounts

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const storeController = require('../controllers/storeController');
+const userController = require('../controllers/userController');
 
 const { catchErrors } = require('../handlers/errorHandlers');
 
@@ -27,7 +28,20 @@ router.get('/stores/:id/edit', catchErrors(storeController.editStore));
 // Show store
 router.get('/store/:slug', catchErrors(storeController.getStoreBySlug));
 
-router.get('/tags', catchErrors(storeController.getStoreByTag))
-router.get('/tags/:tag', catchErrors(storeController.getStoreByTag))
+// Tags page
+router.get('/tags', catchErrors(storeController.getStoreByTag));
+router.get('/tags/:tag', catchErrors(storeController.getStoreByTag));
+
+// Login and register
+router.get('/login', catchErrors(userController.loginForm));
+router.get('/register', catchErrors(userController.registerForm));
+
+// 1. Validate the registration data
+// 2. Register the user
+// 3. Log in user
+router.post('/register',
+	userController.validateRegister,
+	catchErrors(userController.registerForm)
+);
 
 module.exports = router;
